784e0f9d95e2ae156a3dd774b953bef1f356ddb2
atbk2000 <antoniothommaz207@gmail.com>
2023-05-24 14:27:43 -0300

    Testando leitura do acelerômetro/giroscópio

--- classificador_pessoas/src/main.cpp
+++ classificador_pessoas/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 Adafruit_MPU6050 mpu;
@@ -14,7 +13,7 @@ void setup(void)
     }
 
     Serial.println("Adafruit MPU6050 test!");
-
+    
     // Try to initialize!
     if (!mpu.begin())
     {     
@@ -24,74 +23,19 @@ void setup(void)
             delay(10);
         }
     }
-
     Serial.println("MPU6050 Found!");
 
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    Serial.print("Accelerometer range set to: ");
-    switch (mpu.getAccelerometerRange())
-    {
-    case MPU6050_RANGE_2_G:
-        Serial.println("+-2G");
-        break;
-    case MPU6050_RANGE_4_G:
-        Serial.println("+-4G");
-        break;
-    case MPU6050_RANGE_8_G:
-        Serial.println("+-8G");
-        break;
-    case MPU6050_RANGE_16_G:
-        Serial.println("+-16G");
-        break;
-    }
+    // Set accelerometer range (Gs)
+    mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
 
+    // Set gyro range (rad/s)
     mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    Serial.print("Gyro range set to: ");
-    switch (mpu.getGyroRange())
-    {
-    case MPU6050_RANGE_250_DEG:
-        Serial.println("+- 250 deg/s");
-        break;
-    case MPU6050_RANGE_500_DEG:
-        Serial.println("+- 500 deg/s");
-        break;
-    case MPU6050_RANGE_1000_DEG:
-        Serial.println("+- 1000 deg/s");
-        break;
-    case MPU6050_RANGE_2000_DEG:
-        Serial.println("+- 2000 deg/s");
-        break;
-    }
 
+    // Set filter bandwith
     mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-    Serial.print("Filter bandwidth set to: ");
-    switch (mpu.getFilterBandwidth())
-    {
-    case MPU6050_BAND_260_HZ:
-        Serial.println("260 Hz");
-        break;
-    case MPU6050_BAND_184_HZ:
-        Serial.println("184 Hz");
-        break;
-    case MPU6050_BAND_94_HZ:
-        Serial.println("94 Hz");
-        break;
-    case MPU6050_BAND_44_HZ:
-        Serial.println("44 Hz");
-        break;
-    case MPU6050_BAND_21_HZ:
-        Serial.println("21 Hz");
-        break;
-    case MPU6050_BAND_10_HZ:
-        Serial.println("10 Hz");
-        break;
-    case MPU6050_BAND_5_HZ:
-        Serial.println("5 Hz");
-        break;
-    }
 
     Serial.println("");
-    delay(100);
+    delay(200);
 }
 
 void loop()
@@ -117,10 +61,6 @@ void loop()
     Serial.print(g.gyro.z);
     Serial.println(" rad/s");
 
-    Serial.print("Temperature: ");
-    Serial.print(temp.temperature);
-    Serial.println(" degC");
-
     Serial.println("");
-    delay(500);
+    delay(1000);
 }
